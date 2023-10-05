1cee0cc58fa94db95f6fd0d339d002f57c9dd20f
Ayushraj Khare <ayushrajkhare5@gmail.com>
2023-10-05 20:11:34 +0530

    Backend Code For Production

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const fileUpload = require('express-fileupload');
+const { API_PORT } = require('./Constant');
+const code_conversion_router = require('./Routes/CodeConversion.router');
+const codeExplanationRouter = require('./Routes/CodeExplanation.router');
+const commentComposerRouter = require('./Routes/CommentComposer.router');
+const app = express();
+const cors = require('cors');
+app.use(cors({
+    origin: '*'
+}));
+
+app.use(express.json());
+app.use(fileUpload())
+
+
+app.use("/codeconversion", code_conversion_router)
+app.use('/commentcomposer', commentComposerRouter);
+app.use('/codeexplanation', codeExplanationRouter);
+
+
+app.listen(API_PORT, () => {
+    console.log('Server is running on port', API_PORT);
+
+})
